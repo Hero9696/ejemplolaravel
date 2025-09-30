265b9911145f2fc27fcae0eefc94fade3b4fd01b
Hero9696 <miguelpadilla9619@gmail.com>
2025-09-29 22:23:11 -0600

    Model create

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -14,6 +14,7 @@ export default function Welcome() {
                     rel="stylesheet"
                 />
             </Head>
+            <h1>Welcome to Laravel</h1>
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
                 <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
                     <nav className="flex items-center justify-end gap-4">
