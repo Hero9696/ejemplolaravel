498483a13e571ddf7fce07c1875ee10e3dbdb2d5
Hero9696 <miguelpadilla9619@gmail.com>
2025-09-29 22:34:01 -0600

    Migrations

--- database/migrations/2025_09_30_040938_create_products_table.php
+++ database/migrations/2025_09_30_040938_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedInteger('stock')->default(0);
+            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
